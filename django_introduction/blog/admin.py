4c085ed74541f6a8856e2a0c9b30919c558270aa
gaoaolei <853573584@qq.com>
2019-07-21 14:51:27 +0800

    add new

--- django_introduction/blog/admin.py
+++ django_introduction/blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from  blog import models
 # Register your models here.
-admin.site.register(models.Article)
+# admin.site.register(models.Article)
+class A(admin.ModelAdmin):
+    list_display = ('article_id','title','brief_content','content','publish_time')
+    list_filter = ('title',)
+    list_per_page = 3
+    ordering = ('article_id',)
+    search_fields = ('title',)
+
+    #自定义表单（add页面）
+    fields = ('title', 'brief_content')
+admin.site.register(models.Article, A)
--- django_introduction/django_introduction/settings.py
+++ django_introduction/django_introduction/settings.py
@@ -111,8 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
