2c35a31b25cc8f0fb3c192cfc6f359d3b15892ea
gaoaolei <gaoaolei@km.com>
2021-11-21 20:47:03 +0800

    'adsf'

--- 编程-flask/flask接口.py
+++ 编程-flask/flask接口.py
@@ -4,13 +4,13 @@ from flask import request
 
 server = flask.Flask(__name__)
 
-username = request.values.get('name')
-pwd = request.values.get('pwd')
+
 # headers = request.headers
 
 @server.route('/login',methods=['get','post'])
 def login():
-
+    username = request.values.get('name')
+    pwd = request.values.get('pwd')
     if username and pwd:
         if username == 'xiaoming' and pwd == '111':
             result = {"code":200,'message':'登录成功'}
@@ -20,7 +20,7 @@ def login():
             return json.dumps(result,ensure_ascii=False)
     else:
         result = {"code":10001,'message':'参数不能为空'}
-        return json.dumps(result,ensure_ascii=False)
+        return json.dumps(result,ensure_ascii=False),200,[('city','shanghai')]
 
 
 
